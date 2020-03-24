6a97e80826e28f2949ef124146974847a6375de2
Diego Coello <diego.coello@sitechhs.com>
2020-03-24 14:46:03 -0400

    Intro to JavaScript!!!

---
+++ index.js
@@ -0,0 +1,45 @@
+let string = "strings";
+let sentence = "Hello " + "Junshen";
+let sentence2 = " and Ema";
+//console.log(sentence + sentence2);
+
+/*const longSentence = `${student1} and ${student2} are amazing and will be taking ${courseName} with ${teacherName}`; //Template literal or Template String
+let url = ${url}${movie}  */
+
+
+////Numbers or Integers
+let year = 2020;
+let nextYear = 2020 + 1;
+let past = 2020 / 2;
+let farFuture = 2020 * 50;
+
+//////////Booleans
+let trueStatement = true;
+let falseStatement = false;
+
+/////Null and Undefined
+//let Ron; //declared Ron but not defined Ron
+//console.log(Sarah); used for debugging
+/////console.log(Sarah);
+
+/////////Boolean Logic
+let student = "Caroline";
+let age = 15;
+//console.log(student === "Caroline");
+//console.log(student === "caroline");
+//console.log(age === "15");
+//console.log(age == "15");
+/*console.log(age <= 15 || age > 19);
+console.log(age <= 15 && age > 19);*/
+
+//////If statements
+let school = "Elementary";
+if(school === "Elementary") {
+    console.log("You are in k-5");
+} else if(school === "Middle"){
+    console.log("You are ion 6,7 or 8th garde");
+} else {
+    console.log("you are in HS");
+}
+let ron = "Ron";
+console.log(ron !== "Ema");
