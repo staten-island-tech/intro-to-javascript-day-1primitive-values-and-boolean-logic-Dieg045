3aa55fd5e3994f723bb9691b18f6a80b4e4c503e
Diego Coello <diego.coello@sitechhs.com>
2020-03-27 14:15:22 -0400

    Notes Taken 3.27.20

--- index.js
+++ index.js
@@ -45,17 +45,17 @@ let ron = "Ron";
 console.log(ron !== "Ema");*/
 
 //Challenge 1
-let lightColor = "green";
+/*let lightColor = "green";
 if (lightColor === "green") {
     console.log("Go!");
 } else if (lightColor === "yellow") {
     console.log("Caution!");
 } else {
     console.log("STOP!");
-}
+}*/
 
 //Challenge 2 
-let kristyTollerance = true;
+/*let kristyTollerance = true;
 let baoBaoTollerance = true;
 if (kristyTollerance === true && baoBaoTollerance === true) {
     console.log("Only Milk");
@@ -63,10 +63,10 @@ if (kristyTollerance === true && baoBaoTollerance === true) {
     console.log("Get both milks");
 } else {
     console.log("Only Almond Milk");
-}
+}*/
 
 //Challenge 3
-let temp = 67;
+/*let temp = 67;
 if (temp < 68) {
     console.log("Too Cold!");
 } else if (temp >= 68 && temp <= 72) {
@@ -75,4 +75,60 @@ if (temp < 68) {
     console.log("Warm");
 } else {
     console.log("Too Hot!");
+}*/
+
+//////functions
+/*function test () {
+    console.log("TEST");
 }
+test();*/
+
+/*function argument(name1, name2) {
+    console.log(name1, name2);
+}
+argument("val", "eric");*/
+
+/*const add = function(x,y) {
+    return x + y; //immediately ends the function
+}
+console.log(add(10, 3));*/
+/*const hello = function() {
+    alert("hello");
+};
+const bye = function() {
+    alert("Bye");
+};
+
+const beatles = function() {
+    hello();
+    bye();
+    hello();
+    bye();
+};
+beatles();*/
+
+///scope
+/*const name = "Michael y"; //global space
+function makeFunc () {
+    const name = "Sun"; //function scope
+    function displayName() {
+        alert(name); //inner function can access outer function
+    }
+    return displayName; //returning the inner function;
+}
+const myFunc = makeFunc();
+myFunc();*/
+
+//////////arrays
+const names = ["Diego", "Frid", "Christian", "Valerie", "Haoran", "Brian", "Caroline"];
+//names.push("Caroline");
+names.forEach(el => console.log(el));
+//names.filter(//somecode).forEach(el () => console.log(el))
+
+///spread operator
+const numbers = [1, 2, 3];
+
+const add = function (x, y, z) {
+    return x + y;
+};
+console.log(add(...numbers));
